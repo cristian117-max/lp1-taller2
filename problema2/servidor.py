c8d080b134de5d85b37335113800491b20b86c9f
cristian117-max <cristianibanez1422@gmail.com>
2026-02-26 19:07:35 -0500

    solucion problema 2

--- problema2/servidor.py
+++ problema2/servidor.py
@@ -1,44 +1,24 @@
 #!/usr/bin/env python3
 """
-Problema 2: Comunicación bidireccional - Servidor
-Objetivo: Crear un servidor TCP que devuelva exactamente lo que recibe del cliente
+Problema 2: Comunicación bidireccional - Cliente
+Objetivo: Crear un cliente TCP que envíe un mensaje al servidor y reciba la misma respuesta
 """
 
 import socket
+HOST = "localhost"
+PORT = 9000
 
-# TODO: Definir la dirección y puerto del servidor
 
-# TODO: Crear un socket TCP/IP
-# AF_INET: socket de familia IPv4
-# SOCK_STREAM: socket de tipo TCP (orientado a conexión)
+mensaje = input("Digite tu mensage: ")
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+cliente.connect((HOST, PORT))
 
-# TODO: Enlazar el socket a la dirección y puerto especificados
 
-# TODO: Poner el socket en modo escucha
-# El parámetro define el número máximo de conexiones en cola
+cliente.sendall(mensaje.encode())
+print(f"Mensaje enviados: '{mensaje}'")    
 
-# Bucle infinito para manejar múltiples conexiones (una a la vez)
-while True:
 
-    print("Servidor a la espera de conexiones ...")
-    
-    # TODO: Aceptar una conexión entrante
-    # accept() bloquea hasta que llega una conexión
-    # conn: nuevo socket para comunicarse con el cliente
-    # addr: dirección y puerto del cliente
-    
-    print(f"Conexión realizada por {addr}")
+respuesta = cliente.recv(1024) 
+print(f"Respuesta del 'Echo': '{respuesta.decode()}'") 
 
-    # TODO: Recibir datos del cliente (hasta 1024 bytes)
-    
-    # Si no se reciben datos, salir del bucle
-    if not data:
-        break
-
-    # Mostrar los datos recibidos (en formato bytes)
-    print("Datos recibidos:", data)
-    
-    # TODO: Enviar los mismos datos de vuelta al cliente (echo)
-    
-    # TODO: Cerrar la conexión con el cliente actual
-
+cliente.close()
