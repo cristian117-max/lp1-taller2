0dc4d55af541838281086e2178ec258c6965db82
cristian117-max <cristianibanez1422@gmail.com>
2026-02-26 19:11:04 -0500

    solucion problema 4

--- problema4/cliente.py
+++ problema4/cliente.py
@@ -3,27 +3,16 @@
 Problema 4: Servidor HTTP básico - Cliente
 Objetivo: Crear un cliente HTTP que realice una petición GET a un servidor web local
 """
-
+import socket
 import http.client
 
-# TODO: Definir la dirección y puerto del servidor HTTP
+HOST = 'localhost'
+PORT = 9000
 
-# TODO: Crear una conexión HTTP con el servidor
-# HTTPConnection permite establecer conexiones HTTP con servidores
+cliente = http.client.HTTPConnection(HOST, PORT)
+cliente.request("GET", "/")
+respuesta = cliente.getresponse()
+datos = respuesta.read().decode()
+print(datos)
 
-# TODO: Realizar una petición GET al path raíz ('/')
-# request() envía la petición HTTP al servidor
-# Primer parámetro: método HTTP (GET, POST, etc.)
-# Segundo parámetro: path del recurso solicitado
-
-# TODO: Obtener la respuesta del servidor
-# getresponse() devuelve un objeto HTTPResponse con los datos de la respuesta
-
-# TODO: Leer el contenido de la respuesta
-# read() devuelve el cuerpo de la respuesta en bytes
-
-# TODO: Decodificar los datos de bytes a string e imprimirlos
-# decode() convierte los bytes a string usando UTF-8 por defecto
-
-# TODO: Cerrar la conexión con el servidor
-
+cliente.close()
