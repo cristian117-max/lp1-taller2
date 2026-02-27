1a8b7be46eb78fcac08251f2579af36195474cab
cristian117-max <cristianibanez1422@gmail.com>
2026-02-26 19:05:09 -0500

    solucion problema 1

--- problema1/cliente.py
+++ problema1/cliente.py
@@ -5,19 +5,18 @@ Objetivo: Crear un cliente TCP que se conecte a un servidor e intercambie mensaj
 """
 
 import socket
+HOST = 'Localhost'
+PORT = 9000
 
-# TODO: Crear un socket TCP/IP
-# AF_INET: socket de familia IPv4
-# SOCK_STREAM: socket de tipo TCP (orientado a conexión)
 
-# TODO: Conectar el socket al servidor en la dirección y puerto especificados
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# TODO: Enviar datos al servidor (convertidos a bytes)
-# sendall() asegura que todos los datos sean enviados
+cliente.connect((HOST, PORT))
 
-# TODO: Recibir datos del servidor (hasta 1024 bytes)
+cliente.sendall(b"Mundo!") 
 
-# TODO: Decodificar e imprimir los datos recibidos
+respuesta = cliente.recv(1024)
 
-# TODO: Cerrar la conexión con el servidor
+print(f"respuesta: {respuesta}")
 
+cliente.close()
